a6dc3700fa2d538824ba5e90be4753e52165b2bd
wajdi ben yaghlene <110338106+wajdi-cpu@users.noreply.github.com>
2025-12-30 09:39:26 +0100

    Add argument parsing for host and port

--- ASCCI_build_nc.py
+++ ASCCI_build_nc.py
@@ -1,27 +1,35 @@
 import subprocess
-
+import argparse
 try:
-    # netcat part 
-    port = "49419"
-    host = "wily-courier.picoctf.net"
-    output = []
-    flag = ""
-    command = ["nc", host, port]
-    res = subprocess.run(
-        command,
-        capture_output=True,
-        timeout=10,
-        text=True
-    ) 
-    for line in res.stdout.strip().splitlines():
-        line = line.strip()
-        if line.isdigit():          
-            output.append(int(line))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host_arg",help="give the host ")
+    parser.add_argument("port_arg",help="give the port ")
+    args=parser.parse_args()
 
-    for flag_part in output:
-        flag = flag + chr(flag_part)
-        
-    print("This is the Flag :", flag)
-except Exception as e:
-    print("Error in the opening of the nc session !!")
-    print("Details:", e)
+    try:
+        # netcat part 
+        port = args.port_arg
+        host = args.host_arg
+        output = []
+        flag = ""
+        command = ["nc", host, port]
+        res = subprocess.run(
+            command,
+            capture_output=True,
+            timeout=10,
+            text=True
+        ) 
+        for line in res.stdout.strip().splitlines():
+            line = line.strip()
+            if line.isdigit():          
+                output.append(int(line))
+
+        for flag_part in output:
+            flag = flag + chr(flag_part)
+            
+        print("This is the Flag :", flag)
+    except Exception as e:
+        print("Error in the opening of the nc session !!")
+        print("Details:", e)
+except:
+    print("usage : python3 script_name.py <host> <port>")
